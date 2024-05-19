4fda8d8b2c5783551a347f6e8dbb8d1774d34c32
NikuUniversity <NikuUniversity@gmail.com>
2024-05-19 12:52:27 +0530

    NGXS Counter

--- NGXSProject/src/app/app.module.ts
+++ NGXSProject/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxsDataPluginModule } from '@ngxs-labs/data';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountState } from './count.state';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot([CountState]),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsDataPluginModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
